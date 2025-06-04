257f83910dee5ab45301c2261ba23589ca6de005
shebwell <shebazwell@outlook.com>
2025-06-03 22:30:46 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,28 @@
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "VPC CIDR block"
+  default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_az1" {
-  default = "10.0.1.0/24"
+variable "public_subnet_az1_cidr" {
+  description = "Public Subnet AZ1"
+  default     = "10.0.1.0/24"
 }
 
-variable "public_subnet_az2" {
-  default = "10.0.2.0/24"
+variable "public_subnet_az2_cidr" {
+  description = "Public Subnet AZ2"
+  default     = "10.0.2.0/24"
+}
+
+variable "region" {
+  description = "AWS Region"
+  default     = "us-east-1"
 }
 
 variable "instance_type" {
-  default = "t3.medium"
+  description = "EC2 Instance Type"
+  default     = "t3.medium"
 }
 
 variable "key_name" {
-  description = "EC2 Key Pair name"
-  type        = string
+  description = "Name of the existing EC2 Key Pair"
 }
